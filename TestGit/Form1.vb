573eea15a4d957bcc5be09a55f576f059728ee67
Thraann <blackflame_1995@yahoo.com>
2014-10-29 08:55:48 -0500

    words

--- TestGit/Form1.vb
+++ TestGit/Form1.vb
@@ -1,3 +1,6 @@
 ﻿Public Class Form1
 
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        TextBox1.Text = "bruh"
+    End Sub
 End Class
